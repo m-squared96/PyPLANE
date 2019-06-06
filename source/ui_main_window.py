ed97fb6379ec07826fd716b25b0edd13a7c49430
WalJT <jttw259@zoho.com>
2019-06-06 11:10:30 +0100

    GUI can now plot phase space from entered equations

--- source/ui_main_window.py
+++ source/ui_main_window.py
@@ -1,6 +1,9 @@
 """
 Draws the main window of the PyPLANE Qt5 interface
 """
+# TODO: Take user input for params
+# TODO: Take user input for phase_coords
+# TODO: Take user input for t_f and t_r
 import sys
 from PyQt5.QtWidgets import (
     QApplication,
@@ -57,9 +60,30 @@ class MainWindow(QWidget):
 
         self.setLayout(overall_layout)
 
+        # Button Actions
+        self.plot_button.clicked.connect(self.plot_button_clicked)
+
         # Set window title and show
         self.setWindowTitle("PyPLANE Main Window")
         self.show()
+
+    def plot_button_clicked(self):
+        """
+        Plot the phase space when the 'plot' button is clicked
+        """
+        f_1 = self.x_prime_entry.text()
+        f_2 = self.y_prime_entry.text()
+        #print(f_1, f_2)
+
+        phase_coords = ['x', 'y']
+        eqns = [f_1, f_2]
+        params = {'a': -1, 'b': 5, 'c': -4, 'd': -2}
+        t_f = 5
+        t_r = -5
+
+        system_of_eqns = SystemOfEquations(phase_coords, eqns, params=params)
+        plotter = PhaseSpacePlotter(system_of_eqns, t_f, t_r)
+        
 
 if __name__ == "__main__":
     PyPLANE = QApplication(sys.argv)
