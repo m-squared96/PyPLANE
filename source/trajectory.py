0080f9aa22d601614f928c427d91dda0cdb3ce58
m-squared96 <michael.moore34@mail.dcu.ie>
2019-06-13 08:42:19 +0100

    Code better documented. One dimensional plotting almost ready. Issues with quiver directions and step sizes.

--- source/trajectory.py
+++ source/trajectory.py
@@ -39,9 +39,20 @@ class PhaseSpacePlotter(object):
             X, Y = np.meshgrid(np.linspace(xmin * (1 + self.quiver_expansion_factor), xmax * (1 + self.quiver_expansion_factor), 15),
                                np.linspace(ymin * (1 + self.quiver_expansion_factor), ymax * (1 + self.quiver_expansion_factor), 15))
 
-            eval_vector = self.derivative_expression_resolve(display_vars, dimensions, (X, Y))
-            U, V = self.system.phasespace_eval(display_vars, eval_vector)
+            # If only single dimension plotted, time on the x-axis of the plot. dt/dt = 1, hence U is a mesh of ones.
+            # V is the variable's derivative mesh, as is normally the case for a quiverplot
+            if dimensions == 1:
+                U = np.ones(X.shape)
+                V = self.system.phasespace_eval(display_vars, (Y,))
 
+            # If 2D system visualised, U and V are the usual meshes of variable derivatives at the respective points
+            # in phase space.
+            elif dimensions == 2:
+                # For a system with system coords [w, x, y, z], eval_vector will be a NumPy array with coordinates
+                # [w, x, y, z]
+                eval_vector = self.derivative_expression_resolve(display_vars, dimensions, (X, Y))
+                U, V = self.system.phasespace_eval(display_vars, eval_vector)
+        
             # Sets up quiver plot
             self.quiver = self.ax.quiver(X, Y, U, V, pivot="middle")
             self.trajectory = self.ax.plot(0, 0) # Need an initial 'trajectory'
@@ -54,6 +65,12 @@ class PhaseSpacePlotter(object):
             if not(var in self.system.system_coords): return 
 
         self.fig = plt.figure()
+        # Display vars are the system variables to be plotted. 
+        # Given a system with coords [x, y], display_vars can take 4 forms:
+        # 1. ["x"], 2. ["y"], 3. ["x", "y"], 4. ["y", "x"]
+        # In the one-dimensional cases, the quiverplot will be t vs x, or t vs y.
+        # In the two-dimensional cases, the variables plotted on a given axis depend on the order of display_vars.
+        # If display_vars = ["y", "x"] the y variable is plotted on the x-axis, and x on the y-axis. Trippy, I know.
         self.display_vars = display_vars
         self.dimensions = len(self.display_vars)
 
@@ -80,37 +97,79 @@ class PhaseSpacePlotter(object):
         x_event = event.xdata
         y_event = event.ydata
 
-        self.ax.plot(x_event, y_event, ls="", marker="x", c="#FF0000")
+        self.ax.plot(x_event, y_event, ls="", marker="x", c="#FF0000") # Plots a red "x" on the position of the user's click
 
-        # Trajectory production and plotting
-        eval_point = self.derivative_expression_resolve(self.display_vars, self.dimensions, (x_event, y_event))
-        solution_f = self.system.solve((0, self.time_f), r0=eval_point)
-        solution_r = self.system.solve((0, self.time_r), r0=eval_point)
+        # Seperate blocks of code for handling one and two dimensions are required as, from an SOE point of view, they are
+        # fundamentally different, as t is not a system variable in the same way that, for example, x is.
+        if self.dimensions == 2:
+            # Trajectory production and plotting
 
-        for sol in (solution_f, solution_r):
-            if sol.success:
-                # sol.y has shape (2, n_points) for a 2-D system
-                # print(len(sol.t))
-                x = sol.y[self.system.system_coords.index(self.display_vars[0]),:]
-                y = sol.y[self.system.system_coords.index(self.display_vars[1]),:]
-                self.trajectory = self.ax.plot(x, y, c="#0066FF")
-                self.fig.canvas.draw()
-            else:
-                print(sol.message)
+            # eval_point is the point on the quiverplot that has been clicked by the user. However, the coordinates are made
+            # consistent with the ordering of the system coordinates. For example, on a graph with y vs x, the x_event variable
+            # will correspond to a value on x-axis, which represents the y variable of the system. Similarly with the y_event variable
+            # representing the x variable of the system. In this case, eval_point = (y_event, x_event) such that the coordinates
+            # are in the order (x, y) for the SOE to solve and evaluate.
+            eval_point = self.derivative_expression_resolve(self.display_vars, self.dimensions, (x_event, y_event))
+            solution_f = self.system.solve((0, self.time_f), r0=eval_point)
+            solution_r = self.system.solve((0, self.time_r), r0=eval_point)
+
+            for sol in (solution_f, solution_r):
+                if sol.success:
+                    # sol.y has shape (2, n_points) for a 2-D system
+                    # print(len(sol.t))
+                    x = sol.y[self.system.system_coords.index(self.display_vars[0]),:]
+                    y = sol.y[self.system.system_coords.index(self.display_vars[1]),:]
+                    self.trajectory = self.ax.plot(x, y, c="#0066FF")
+                    self.fig.canvas.draw()
+                else: print(sol.message)
+        
+        elif self.dimensions == 1:
+            # Recall that in a 1D scenario, the x_event variable is essentially the inital time of the trajectory
+            solution_f = self.system.solve((x_event, self.time_f), r0=[y_event])
+            solution_r = self.system.solve((x_event, self.time_r), r0=[y_event])
+            for sol, t in zip((solution_f, solution_r), (self.time_f, self.time_r)):
+                if sol.success:
+                    y = sol.y[0, :]
+                    if x_event != t: x = np.linspace(x_event, t, y.size)
+                    elif x_event == t: x = x_event
+                    self.trajectory = self.ax.plot(x, y, c="#0066FF")
+                    self.fig.canvas.draw()
+                else:
+                    print(sol.message)
 
         self.trajectory_count += 1
 
     def derivative_expression_resolve(self, display_vars, dimensions, positions):
+        """
+        Function to resolve the coordinates of an argument to the order of
+        coordinates in an equations.SystemOfEquations object
+        """
         eval_seq = []
-        for var in self.system.system_coords:
-            if not(var in display_vars): eval_seq.append(0)
-            
-            elif var in display_vars:
-                eval_seq.append(positions[display_vars.index(var)])
+        
+        if dimensions in (2, 3):
+            for var in self.system.system_coords:
+                if not(var in display_vars): eval_seq.append(0)
+                
+                elif var in display_vars:
+                    eval_seq.append(positions[display_vars.index(var)])
+        
+        elif dimensions == 1:
+            eval_seq.append(self.system.system_coords[0])
 
         return np.array(eval_seq)
 
-def example():
+def one_D_example():
+    phase_coords = ['x']
+    eqns = ['x^2']
+    params = {'a':1, 'b':0}
+    t_f = 20
+    t_r = -20
+
+    sys = SystemOfEquations(phase_coords, eqns, params=params)
+    plotter = PhaseSpacePlotter(sys, t_f, t_r)
+    plotter.show_plot(['x'], np.array(((0, 10), (0, 2))))
+
+def two_D_example():
     phase_coords = ['x', 'y']
     eqns = [
         'ax + by',
@@ -130,4 +189,5 @@ def example():
     plotter.show_plot(['x', 'y'], np.array(((-10, 10), (-10, 10))))
 
 if __name__ == "__main__":
-    example()
+    one_D_example()
+    two_D_example()
