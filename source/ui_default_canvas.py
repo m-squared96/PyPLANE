a0ca212346465dd9e91b6b6f1fdadddd7d528dfb
WalJT <jttw259@zoho.com>
2019-07-05 18:26:51 +0100

    Started using Flake8 linter... fixed a few issues it threw up.

--- source/ui_default_canvas.py
+++ source/ui_default_canvas.py
@@ -6,11 +6,13 @@ Create a sample canvas that used to plot in the main window
 from trajectory import PhaseSpacePlotter
 from equations import SystemOfEquations
 
+
 class DefaultCanvas(PhaseSpacePlotter):
-    
+
     def __init__(self):
-        self.default_system = SystemOfEquations(["x", "y"], ["y*sin(x)", "-x"], params={})
-        
+        self.default_system = SystemOfEquations(["x", "y"],
+            ["y*sin(x)", "-x"], params={})
+
         self.display_vars = self.default_system.system_coords
         self.dimensions = len(self.display_vars)
 
