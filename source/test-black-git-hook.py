bac965df0daef618fc7990851a9dd19fb646254e
Maksim Jancenko <maksimjancenko@gmail.com>
2019-09-14 13:38:52 +0100

    added test file for formatter pre-commit hook (REMOVE BEFORE MERGE)

---
+++ source/test-black-git-hook.py
@@ -0,0 +1,20 @@
+# just used to test that the Black formatter git hook works as expected.
+# This is not part of the main source code of the project.
+
+# Before formatting:
+# a=1
+# b = [1, 2, 3,
+#      4, 5
+#      ]
+
+# if a==0: print('a is 0')
+# else: print('a is 1')
+
+
+a = 1
+b = [1, 2, 3, 4, 5]
+
+if a == 0:
+    print("a is 0")
+else:
+    print("a is 1")
