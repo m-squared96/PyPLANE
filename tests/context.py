834f9c17fb982b4603d6d5437d41f075bec92700
WalJT <waljt@pm.me>
2020-03-18 11:32:30 +0000

    Modified tests to account for directory renaming (source -> PyPLANE)

--- tests/context.py
+++ tests/context.py
@@ -4,4 +4,4 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import source
+import PyPLANE
--- tests/test_SystemOfEquations.py
+++ tests/test_SystemOfEquations.py
@@ -4,9 +4,9 @@ import sympy as sp
 from sympy import symbols
 
 import context
-from source.equations import DifferentialEquation
+from PyPLANE.equations import DifferentialEquation
 
-from source.equations import SystemOfEquations
+from PyPLANE.equations import SystemOfEquations
 
 
 class TestSystemOfEquations(unittest.TestCase):
