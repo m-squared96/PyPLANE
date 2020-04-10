71ff5bf8b973f892f4e86c01f4243f304df66a90
WalJT <waljt@pm.me>
2020-04-10 20:51:15 +0100

    Made button big and red

--- PyPLANE_styles/styles.py
+++ PyPLANE_styles/styles.py
@@ -1,6 +1,8 @@
+# CSS to generate the styles
+
 default_light = """
 QWidget {
-  background-color: #cad3f5;
+  background-color: #cbdce5;
   padding: 0px;
   color:  black;
   selection-background-color: pink;
@@ -8,9 +10,18 @@ QWidget {
 }
 
 QLineEdit {
-    color: black;
-    border: 2px solid grey;
-    border-radius: 5px;
-    background-color: white;
+  color: black;
+  border: 2px solid grey;
+  border-radius: 5px;
+  background-color: white;
+}
+
+QPushButton {
+  background-color: red;
+  border: 1px solid grey;
+  color: white;
+  border-radius: 4px;
+  padding: 3px;
+  min-width: 80px;
 }
 """
