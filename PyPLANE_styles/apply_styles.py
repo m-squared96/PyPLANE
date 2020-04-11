b38fbf5108ea5f507b3b7b98949676ab55a48e24
WalJT <waljt@pm.me>
2020-04-11 18:58:33 +0100

    Added a dark style

--- PyPLANE_styles/apply_styles.py
+++ PyPLANE_styles/apply_styles.py
@@ -4,15 +4,17 @@ import os
 
 module_dir = os.path.dirname(__file__)
 
-def default_light_style():
+def default_light():
     """
     The default style that PyPLANE will most likely ship with
     """
     css_file = os.path.abspath(os.path.join(module_dir, "default_light.css"))
     return open(css_file).read()
 
-def default_dark_style():
+def default_dark():
     """
     Because dark themes are awesome
     """
+    css_file = os.path.abspath(os.path.join(module_dir, "default_dark.css"))
+    return open(css_file).read()
     pass
