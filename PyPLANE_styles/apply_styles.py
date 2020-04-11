d6581e992c5014ea103b3620e97a652d0bf6df42
WalJT <waljt@pm.me>
2020-04-11 17:55:07 +0100

    Removed redundant `styles.py` file

--- PyPLANE_styles/apply_styles.py
+++ PyPLANE_styles/apply_styles.py
@@ -1,13 +1,13 @@
-from PyPLANE_styles import styles
 import os
 
- # Define methods to return specific style sheets
+# Define methods to return specific style sheets
+
+module_dir = os.path.dirname(__file__)
 
 def default_light_style():
     """
     The default style that PyPLANE will most likely ship with
     """
-    module_dir = os.path.dirname(__file__)
     css_file = os.path.abspath(os.path.join(module_dir, "default_light.css"))
     return open(css_file).read()
 
