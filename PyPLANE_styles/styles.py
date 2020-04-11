9431fba10e176cbb0417810cfc6116c49ae95bd6
WalJT <waljt@pm.me>
2020-04-11 09:55:47 +0100

    Plot button changes shade of red on hover

--- PyPLANE_styles/styles.py
+++ PyPLANE_styles/styles.py
@@ -11,13 +11,24 @@ QWidget {
 
 QLineEdit {
   color: black;
-  border: 2px solid grey;
+  border: 2px solid #2C5E84;
   border-radius: 5px;
   background-color: white;
 }
 
+/* The only button in PyPLANE (so far) is the big red plot button */
+
 QPushButton {
   background-color: darkred;
+  border: 1px solid grey;
+  color: white;
+  border-radius: 4px;
+  padding: 3px;
+  min-width: 80px;
+}
+
+QPushButton:hover {
+  background-color: red;
   border: 1px solid grey;
   color: white;
   border-radius: 4px;
