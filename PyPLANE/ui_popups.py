a309c7058534a814d9e01644520676c9ecf8e4b4
WalJT <jttw259@zoho.com>
2023-01-29 09:48:39 +0000

    testing stuff

--- PyPLANE/ui_popups.py
+++ PyPLANE/ui_popups.py
@@ -5,18 +5,31 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QWidget
 )
+from PyQt5.QtGui import QWindow
 
 class BasicPopup(QMessageBox):
 
     def __init__(self, title_text = "Title Text", message = "Message Text"):
+        
         super().__init__()
+        self.setSizeGripEnabled(True)
+
         self.setWindowTitle(title_text)
         self.setText(message)
         self.setDetailedText('A bunch more text')
         # self.setSizeGripEnabled(True)
-        # self.setMinimumSize(100, 100)
+        self.resize(500, 500)
 
     layout = QVBoxLayout
+
+# class BasicPopup(QWidget):
+
+#     def __init__(self, title_text = "Title Text", message = "Message Text"):
+#         super().__init__()
+#         layout = QVBoxLayout()
+#         self.label = QLabel(title_text)
+#         layout.addWidget(self.label)
+#         self.setLayout(layout)
 
 def display_test_popup():
     test = BasicPopup()
